b058a851d6bfeb1218b7b61d9fe99fa847bb9e07
wsharpe41 <122390836+wsharpe41@users.noreply.github.com>
2023-02-15 15:48:17 +0000

    Added python notebook

--- boston.py
+++ boston.py
@@ -8,3 +8,11 @@ url = "https://pollysnips.s3.amazonaws.com/bostonEmployeeSalaries.json"
 uh = urllib.request.urlopen(url)
 data = uh.read().decode()
 print('Retrieved', len(data), 'characters')
+# print type of data
+print(type(data))
+# convert data to json
+js = json.loads(data)
+# print type of js
+print(type(js))
+data = js["data"]
+print(data[0])
